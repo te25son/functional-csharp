9c5bc3c23c664fe074dd78b3d6adb5c90e363ea3
te25son <te25son@gmail.com>
2020-07-11 08:25:10 +0200

    Add comments

--- Examples/Chapter01/MutationShouldBeAvoided.cs
+++ Examples/Chapter01/MutationShouldBeAvoided.cs
@@ -18,6 +18,8 @@ namespace Examples.Chapter01
             Action task1 = () => WriteLine(nums.Sum());
             Action task2 = () => { nums.Sort(); WriteLine(nums.Sum()); };
 
+            // By sorting the list while concurrently summing up the elements within the same list,
+            // errors will inevitably occur (counting the same element more than once).
             Parallel.Invoke(task1, task2);
         }
 
@@ -28,6 +30,9 @@ namespace Examples.Chapter01
             Action task1 = () => WriteLine(nums.Sum());
             Action task2 = () => { nums.OrderBy(x => x); WriteLine(nums.Sum()); };
 
+            // Unlike Sort, LINQs OrderBy does not mutate the original list. Instead, it creates
+            // a new, modified version of the original list. That way task1 is summing up the elements
+            // of the original list while task2 generates a new ordered list from the original list.
             Parallel.Invoke(task1, task2);
         }
     }
