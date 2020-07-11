534c3277c7d1123a30be9aa951eda696ef77e709
te25son <te25son@gmail.com>
2020-07-11 08:17:06 +0200

    Initialize project with first examples

---
+++ Examples/Chapter01/MutationShouldBeAvoided.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Chapter01
+{
+    using static Enumerable;
+    using static Console;
+
+    public class MutationShouldBeAvoided
+    {
+        public static void WithListItBreaks()
+        {
+            var nums = Range(-10000, 20001).Reverse().ToList();
+
+            Action task1 = () => WriteLine(nums.Sum());
+            Action task2 = () => { nums.Sort(); WriteLine(nums.Sum()); };
+
+            Parallel.Invoke(task1, task2);
+        }
+
+        public static void WithIEnumerableItWorks()
+        {
+            var nums = Range(-10000, 20001).Reverse();
+
+            Action task1 = () => WriteLine(nums.Sum());
+            Action task2 = () => { nums.OrderBy(x => x); WriteLine(nums.Sum()); };
+
+            Parallel.Invoke(task1, task2);
+        }
+    }
+}
