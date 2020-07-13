816b76f6e7bd2fb263e0a2843d0252473817becb
te25son <te25son@gmail.com>
2020-07-13 11:01:06 +0200

    Mutation tests

--- Examples/Chapter01/MutationShouldBeAvoided.cs
+++ Examples/Chapter01/MutationShouldBeAvoided.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,25 @@ namespace Examples.Chapter01
         {
             WithListItBreaks();
             WithIEnumerableItWorks();
+        }
+
+        [Test]
+        public void NoInPlaceUpdates()
+        {
+            var original = new[] { 5, 7, 1 };
+            var sorted = original.OrderBy(x => x).ToList();
+
+            Assert.AreEqual(new[] { 5, 7, 1 }, original);
+            Assert.AreEqual(new[] { 1, 5, 7 }, sorted);
+        }
+
+        [Test]
+        public void InPlaceUpdates()
+        {
+            var original = new List<int> { 5, 7, 1 };
+            original.Sort();
+
+            Assert.AreEqual(new[] { 1, 5, 7 }, original);
         }
 
         public static void WithListItBreaks()
