814fa3251609de394e4e08bd7efb8aa6d2037bee
te25son <te25son@gmail.com>
2020-07-15 08:06:50 +0200

    HOF class / file

---
+++ Examples/Chapter01/HOF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Chapter01
+{
+    public static class HOF
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var enumerable in source)
+            {
+                if (predicate(enumerable))
+                    yield return enumerable;
+            }
+        }
+    }
+}
