04f5d32272ccf0a91a265542f1f3bb94f41b1760
te25son <te25son@gmail.com>
2020-08-26 10:12:05 +0200

    Add ConnectionHelpers to make DbLogger more functional

---
+++ Examples/Chapter01/DbLogger/ConnectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Examples.Chapter01.DbLogger
+{
+    public static class ConnectionHelper
+    {
+        public static R Connect<R>(string connString, Func<IDbConnection, R> func)
+        {
+            using (var conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                return func(conn);
+            }
+        }
+    }
+}
--- Examples/Chapter01/DbLogger/DbLogger.cs
+++ Examples/Chapter01/DbLogger/DbLogger.cs
@@ -6,25 +6,22 @@ using Dapper;
 
 namespace Examples.Chapter01.DbLogger
 {
+    using static ConnectionHelper;
+
     public class DbLogger
     {
         string connString;
 
-        public void Log(LogMessage message)
-        {
-            using (var conn = new SqlConnection(connString))
-            {
-                int affectedRows = conn.Execute("sp_create_log", message, commandType: CommandType.StoredProcedure);
-            }
-        }
+        public void Log(LogMessage message) 
+            => Connect(
+                    connString, 
+                    c => c.Execute("sp_create_log", message, commandType: CommandType.StoredProcedure)
+                );
 
         public IEnumerable<LogMessage> GetLogs(DateTime since)
-        {
-            var sqlGetLogs = "SELECT * FROM [Logs] WHERE [TIMESTAMP] > @since";
-            using (var conn = new SqlConnection(connString))
-            {
-                return conn.Query<LogMessage>(sqlGetLogs, new { since = since });
-            }
-        }
+            => Connect(
+                    connString, 
+                    c => c.Query<LogMessage>(@"SELECT * FROM [Logs] WHERE [TIMESTAMP] > @since", new { since = since })
+               );
     }
 }
