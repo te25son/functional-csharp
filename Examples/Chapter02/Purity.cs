3cc5eee281cd9c1804e4aa80a6ccd896c9420048
te25son <te25son@gmail.com>
2020-09-07 07:55:44 +0200

    Add chapter example folder and purity class

---
+++ Examples/Chapter02/Purity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Examples.Chapter02
+{
+    using static Console;
+
+    public static class Purity
+    {
+        public static void Run()
+        {
+            // You can isolate the pure, computational parts of your program
+            // from the I/O.
+            WriteLine("Enter your name.");
+            var name = ReadLine();
+            WriteLine(GreetingFor(name));
+        }
+
+        public static string GreetingFor(string name) => $"Hello {name}";
+    }
+}
