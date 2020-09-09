4128eff45b1b59eb7f2672da405c489699489e1e
te25son <te25son@gmail.com>
2020-09-09 20:44:46 +0200

    Add list formatter and string extension classes

---
+++ Examples/Chapter02/ListFormatter/ListFormatter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Examples.Chapter02.ListFormatter
+{
+    using static Console;
+
+    public static class ListFormatter
+    {
+        static int counter;
+
+        static string PrependCounter(string s) => $"{++counter}. {s}";
+
+        public static List<string> Format(List<string> list) =>
+            list.Select(StringExtension.ToSentenceCase)
+                .Select(PrependCounter)
+                .ToList();
+
+        public static void _main()
+        {
+            var shoppingList = new List<string> { "apples", "bread", "coffee", "milk" };
+
+            ListFormatter
+                .Format(shoppingList)
+                .ForEach(WriteLine);
+        }
+    }
+}
---
+++ Examples/Chapter02/ListFormatter/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Chapter02.ListFormatter
+{
+    public static class StringExtension
+    {
+        public static string ToSentenceCase(this string origin) =>
+            origin.ToUpper()[0] + origin.ToLower().Substring(1);
+    }
+}
