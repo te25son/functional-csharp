92470d0c1b416c7163bae7e85bff480223e5aa2e
te25son <te25son@gmail.com>
2020-09-13 12:03:38 +0200

    Add list formatter with parallel zip

---
+++ Examples/Chapter02/ListFormatter/ListFormatter_ParallelZip.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Examples.Chapter02.ListFormatter.Parallel.Zip
+{
+    public static class ListFormatter
+    {
+        public static List<string> Format(List<string> list) =>
+            list.AsParallel()
+                .Select(StringExtension.ToSentenceCase)
+                .Zip(ParallelEnumerable.Range(1, list.Count), (s, i) => $"{i}. {s}")
+                .ToList();
+    }
+}
