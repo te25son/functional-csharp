d11829f47ae852a021164510fe84cbe6a382d730
te25son <te25son@gmail.com>
2020-09-19 08:20:19 +0200

    Add banking tests for date validation

---
+++ Examples.Tests/Chapter02/BankingTests.cs
@@ -0,0 +1,48 @@
+﻿using Examples.Chapter02.Banking;
+using NUnit.Framework;
+using System;
+
+namespace Examples.Tests.Chapter02
+{
+    public sealed class BankingTests : TestFixture
+    {
+        [Test]
+        public void ValidationPasses_WhenTransferDateIsFuture()
+        {
+            Test(
+                arrange: () => (
+                    Transfer: new MakeTransfer { DateUtc = DateTime.UtcNow.AddYears(1) },
+                    Validator: new DateValidator()
+                ),
+                act: arrangeResult => arrangeResult.Validator.IsValid(arrangeResult.Transfer),
+                assert: (arrangeResult, actResult) => Assert.IsTrue(actResult)
+            );
+        }
+
+        [Test]
+        public void ValidationPasses_WhenTransferDateIsNow()
+        {
+            Test(
+                arrange: () => (
+                    Transfer: new MakeTransfer { DateUtc = DateTime.UtcNow },
+                    Validator: new DateValidator()
+                ),
+                act: arrangeResult => arrangeResult.Validator.IsValid(arrangeResult.Transfer),
+                assert: (arrangeResult, actResult) => Assert.IsTrue(actResult)
+            );
+        }
+
+        [Test]
+        public void ValidationFails_WhenTransferDateIsPast()
+        {
+            Test(
+                arrange: () => (
+                    Transfer: new MakeTransfer { DateUtc = DateTime.UtcNow.AddYears(-1) },
+                    Validator: new DateValidator()
+                ),
+                act: arrangeResult => arrangeResult.Validator.IsValid(arrangeResult.Transfer),
+                assert: (arrangeResult, actResult) => Assert.IsFalse(actResult)
+            );
+        }
+    }
+}
