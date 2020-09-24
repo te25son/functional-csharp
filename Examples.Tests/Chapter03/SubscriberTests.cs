60922ef9093b6c5a76aa22edfc55a97466716b27
te25son <te25son@gmail.com>
2020-09-24 08:36:23 +0200

    Subscriber tests

---
+++ Examples.Tests/Chapter03/SubscriberTests.cs
@@ -0,0 +1,39 @@
+﻿using Examples.Chapter03;
+using Functional;
+using NUnit.Framework;
+
+namespace Examples.Tests.Chapter03
+{
+    using static F;
+
+    public sealed class SubscriberTests : TestFixture
+    {
+        [Test]
+        public void SubscriberSendsCorrect_WhenNameIsGiven()
+        {
+            Test(
+                arrange: () =>
+                {
+                    var subscriberName = "John";
+
+                    return (
+                        Subscriber: new Subscriber { Name = subscriberName },
+                        SubscriberName: subscriberName.ToUpper()
+                    );
+                },
+                act: arrangeResult => SubscriberComponent.GreetingFor(arrangeResult.Subscriber),
+                assert: (arrangeResult, actResult) => Assert.AreEqual($"Dear {arrangeResult.SubscriberName}", actResult)
+            );
+        }
+
+        [Test]
+        public void SubscriberSendsCorrectGreeting_WhenNameIsNotGiven()
+        {
+            Test(
+                arrange: () => new Subscriber { Name = null },
+                act: arrangeResult => SubscriberComponent.GreetingFor(arrangeResult),
+                assert: (arrangeResult, actResult) => Assert.AreEqual($"Dear subscriber", actResult)
+            );
+        }
+    }
+}
--- Examples/Chapter03/Subscriber.cs
+++ Examples/Chapter03/Subscriber.cs
@@ -7,8 +7,11 @@ namespace Examples.Chapter03
         public Option<string> Name { get; set; }
 
         public string Email { get; set; }
+    }
 
-        public string GreetingFor(Subscriber subscriber) =>
+    public static class SubscriberComponent
+    {
+        public static string GreetingFor(Subscriber subscriber) =>
             subscriber.Name.Match(
                 () => "Dear subscriber",
                 (name) => $"Dear {name.ToUpper()}");
