64554a9b894cbfca916aa87a8e7156e397b939c6
te25son <te25son@gmail.com>
2020-09-24 09:49:54 +0200

    Add implementation for parsing string to option<int>

---
+++ Functional/Int.cs
@@ -0,0 +1,15 @@
+﻿namespace Functional
+{
+    using static F;
+
+    public static class Int
+    {
+        public static Option<int> Parse(string s)
+        {
+            int result;
+            return int.TryParse(s, out result)
+                ? Some(result)
+                : None;
+        }
+    }
+}
