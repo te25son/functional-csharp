0e3a95e14b07638e961b06ec1d03e17495757959
te25son <te25son@gmail.com>
2020-09-24 08:13:20 +0200

    Add subscriber

---
+++ Examples/Chapter03/Subscriber.cs
@@ -0,0 +1,16 @@
+﻿using Functional;
+
+namespace Examples.Chapter03
+{
+    public class Subscriber
+    {
+        public Option<string> Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string GreetingFor(Subscriber subscriber) =>
+            subscriber.Name.Match(
+                () => "Dear subscriber",
+                (name) => $"Dear {name.ToUpper()}");
+    }
+}
