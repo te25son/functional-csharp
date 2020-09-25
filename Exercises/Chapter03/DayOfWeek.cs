6a797bd5c0950b6a2d500ff84f0f4f559c11cc54
te25son <te25son@gmail.com>
2020-09-25 08:36:57 +0200

    Add exercises folder for chapter03

---
+++ Exercises/Chapter03/DayOfWeek.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Functional;
+
+namespace Exercises.Chapter03
+{
+    using static F;
+    using static Console;
+
+    public enum DayOfWeek
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+
+    public static class Enum
+    {
+        public static Option<T> Parse<T>(this string s)
+            where T : struct
+        => System.Enum.TryParse(s, out T t) ? Some(t) : None;
+    }
+
+    public static class EnumExercise
+    {
+        public static void Run()
+        {
+            var shouldBeSome = Enum.Parse<DayOfWeek>("Friday");
+            var shouldBeNone = Enum.Parse<DayOfWeek>("Freeday");
+
+            shouldBeSome.Match(
+                () => { WriteLine("FAIL: should be Some"); return Unit(); }, 
+                (weekday) => { WriteLine($"PASS: the weekday is {weekday}"); return Unit(); }
+            );
+
+            shouldBeNone.Match(
+                () => { WriteLine("PASS: value is None"); return Unit(); },
+                (weekday) => { WriteLine($"FAIL: the weekday is {weekday}"); return Unit(); }
+            );
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,4 +1,6 @@
-﻿using Exercises.Chapter02.BmiCalculator;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Exercises
 {
@@ -6,7 +8,16 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            BmiCalculator.Run();
+            var exercises = new Dictionary<string, Action>
+            {
+                ["BmiCalc"] = Chapter02.BmiCalculator.BmiCalculator.Run,
+                ["EnumExr"] = Chapter03.EnumExercise.Run
+            };
+
+            foreach (var exercise in exercises.Where(e => e.Key.Equals("EnumExr")))
+            {
+                exercise.Value();
+            }
         }
     }
 }
