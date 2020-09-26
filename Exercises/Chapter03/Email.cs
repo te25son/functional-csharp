29098b19000ea8a4ce4efdc7bcff501c73d583f7
te25son <te25son@gmail.com>
2020-09-26 15:22:38 +0200

    Add email exercises

---
+++ Exercises/Chapter03/Email.cs
@@ -0,0 +1,33 @@
+﻿using Functional;
+using System.Text.RegularExpressions;
+
+namespace Exercises.Chapter03
+{
+    using static F;
+    using static System.Console;
+
+    public class Email
+    {
+        static readonly Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+        private string Value { get; }
+
+        private Email(string value) => Value = value;
+
+        public static Option<Email> Create(string s) =>
+            regex.IsMatch(s)
+                ? Some(new Email(s))
+                : None;
+
+        public static implicit operator string(Email email) => email.Value;
+    }
+
+    public static class EmailExample
+    {
+        public static void Run()
+        {
+            var email = Email.Create("email");
+            WriteLine(email.Match(() => "Email not here", e => $"The email is {e}"));
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -12,10 +12,11 @@ namespace Exercises
             {
                 ["BmiCalc"] = Chapter02.BmiCalculator.BmiCalculator.Run,
                 ["EnumExr"] = Chapter03.EnumExercise.Run,
-                ["Lookup"] = Chapter03.LookupExercise.Run
+                ["Lookup"] = Chapter03.LookupExercise.Run,
+                ["EmailExr"] = Chapter03.EmailExample.Run
             };
 
-            foreach (var exercise in exercises.Where(e => e.Key.Equals("Lookup")))
+            foreach (var exercise in exercises.Where(e => e.Key.Equals("EmailExr")))
             {
                 exercise.Value();
             }
