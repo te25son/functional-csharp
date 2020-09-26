e25a630c39ecf591ed7367433c8217b794b873a5
te25son <te25son@gmail.com>
2020-09-26 17:28:53 +0200

    Add map functions

---
+++ Functional/Core.cs
@@ -0,0 +1,24 @@
+﻿using Functional.Option;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Functional
+{
+    using static F;
+
+    public static class Core
+    {
+        public static IEnumerable<R> Map<T, R>(this IEnumerable<T> ts, Func<T, R> func)
+        {
+            foreach (var t in ts)
+                yield return func(t);
+        }
+
+        public static Option<R> Map<T, R>(this Option<T> optT, Func<T, R> func) =>
+            optT.Match(
+                () => None,
+                t => Some(func(t)));
+    }
+}
