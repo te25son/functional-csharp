7804c7cb0270858fbfa70208c3dc83176db672cf
Timothy Eason <timothy@Timothys-MacBook-Air.local>
2020-09-30 15:33:24 +0200

    Add apple pie example

---
+++ Examples/Chapter04/Apple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Functional;
+
+namespace Examples.Chapter04
+{
+    using static F;
+    using static Console;
+
+    public class Apple
+    {
+    }
+
+    public class ApplePie
+    {
+        public ApplePie(Apple apple)
+        {
+        }
+    }
+
+    public static class ApplePieExample
+    {
+        public static void Run()
+        {
+            Func<Apple, ApplePie> makePie = apple => new ApplePie(apple);
+
+            Option<Apple> basketFull = Some(new Apple());
+            Option<Apple> basketEmpty = None;
+
+            WriteLine(basketFull.Map(makePie).Match(() => "Nothing in the basket.", pie => "Your grandma made a pie."));
+            WriteLine(basketEmpty.Map(makePie).Match(() => "Nothing in the basket.", pie => "Your grandma made a pie."));
+        }
+    }
+}
