ee5b60c4b5d724bb2301a965728427ac822ed5af
te25son <te25son@gmail.com>
2020-10-01 19:28:32 +0200

    Add where example

---
+++ Examples/Chapter04/Natural.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Functional;
+
+namespace Examples.Chapter04
+{
+    using static Console;
+
+    public static class NaturalFilter
+    {
+        public static void Run()
+        {
+            WriteLine(new Natural("2").ToNatural());
+            WriteLine(new Natural("-2").ToNatural());
+            WriteLine(new Natural("0").ToNatural());
+        }
+    }
+
+    public class Natural
+    {
+        private readonly string _value;
+
+        public Natural(string value)
+        {
+            _value = value;
+        }
+
+        private bool IsNatural(int i) => i >= 0;
+
+        public Option<int> ToNatural() => Int.Parse(_value).Where(IsNatural);
+    }
+}
