f019e642ac1817e2127570313383e5a0834e9567
te25son <te25son@gmail.com>
2020-10-10 18:40:03 +0200

    Add some test

---
+++ Functional.Tests/OptionTests.cs
@@ -0,0 +1,21 @@
+﻿using Examples.Tests;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Functional.Tests.Option
+{
+    using static F;
+
+    public sealed class OptionTests : TestFixture
+    {
+        [Test]
+        public void SomeConvertsToEnumerableOfOne()
+        {
+            Test(
+                arrange: _ => Some("thing"),
+                act: arrangeResult => arrangeResult.AsEnumerable(),
+                assert: (arrangeResult, actResult) => Assert.AreEqual(1, actResult.Count())
+            );
+        }
+    }
+}
