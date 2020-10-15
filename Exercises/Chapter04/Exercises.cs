8c08c4d26bb7152a88f3b4172684417f24b3eba2
te25son <te25son@gmail.com>
2020-10-15 19:25:34 +0200

    Set up chapter04 exercise

---
+++ Exercises/Chapter04/Exercises.cs
@@ -0,0 +1,27 @@
+﻿using Functional;
+using System;
+
+namespace Exercises.Chapter04
+{
+    public class Exercises
+    {
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+
+        public Option<WorkPermit> WorkPermit { get; set; }
+
+        public DateTime JoinedOn { get; }
+
+        public Option<DateTime> LeftOn { get; }
+    }
+
+    public struct WorkPermit
+    {
+        public string Number { get; set; }
+
+        public DateTime Expiry { get; set; }
+    }
+}
