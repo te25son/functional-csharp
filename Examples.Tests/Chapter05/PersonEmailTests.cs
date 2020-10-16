3bd2065d8eddb4d9093038542640aadf4ae60c4c
te25son <te25son@gmail.com>
2020-10-16 09:14:48 +0200

    Add tests for getting the email for person

--- Examples.Tests/Chapter05/PersonEmailTests.cs
+++ Examples.Tests/Chapter05/PersonEmailTests.cs
@@ -15,5 +15,13 @@ namespace Examples.Tests.Chapter05
             Test(
                 arrange: _ => new Person(firstName, lastName),
                 act: arrangedData => PersonComponent.AbbreviateName(arrangedData));
+
+        [TestCase("Bilbo", "Baggins", ExpectedResult = "biba@company.com")]
+        [TestCase("123", "456", ExpectedResult = "1245@company.com")]
+        [TestCase("'!><", "#$%^", ExpectedResult = "'!#$@company.com")]
+        public string CanGetEmailFor(string firstName, string lastName) =>
+            Test(
+                arrange: _ => new Person(firstName, lastName),
+                act: arrangedData => PersonComponent.EmailFor(arrangedData));
     }
 }
