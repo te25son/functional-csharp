40a4053edbddf0d6d68ed4b9e4efe550724b4f73
te25son <te25son@gmail.com>
2020-10-16 08:47:09 +0200

    Add chapter 05 folder and person example

---
+++ Examples/Chapter05/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Examples.Chapter05
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public class PersonComponent
+    {
+        public Func<Person, string> EmailFor = p => AppendDomain(AbbreviateName(p));
+
+        static string AbbreviateName(Person person) =>
+            Abbreviate(person.FirstName) + Abbreviate(person.LastName);
+
+        static string AppendDomain(string localPart) =>
+            $"{localPart}@company.com";
+
+        static string Abbreviate(string s) =>
+            s.Substring(0, 2).ToLower();
+    }
+}
