6c4ec49b317c01872ee16bc041ec77ef996fba7c
te25son <te25son@gmail.com>
2020-10-16 11:37:40 +0200

    Clean up person component tests

--- Examples.Tests/Chapter05/PersonEmailTests.cs
+++ Examples.Tests/Chapter05/PersonEmailTests.cs
@@ -1,27 +1,42 @@
 ﻿using Examples.Chapter05;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Examples.Tests.Chapter05
 {
+    using static PersonComponent_FuncComp;
+
     public sealed class PersonEmailTests : TestFixture
     {
         [TestCase("Bilbo", "Baggins", ExpectedResult = "biba")]
         [TestCase("123", "456", ExpectedResult = "1245")]
         [TestCase("'!><", "#$%^", ExpectedResult = "'!#$")]
-        public string CanAbbreviateName(string firstName, string lastName) =>
+        public string CanAbbreviateName_UsingFunctionComposition(string firstName, string lastName) =>
             Test(
                 arrange: _ => new Person(firstName, lastName),
-                act: arrangedData => PersonComponent_FuncComp.AbbreviateName(arrangedData));
+                act: arrangedData => AbbreviateName(arrangedData));
 
         [TestCase("Bilbo", "Baggins", ExpectedResult = "biba@company.com")]
         [TestCase("123", "456", ExpectedResult = "1245@company.com")]
         [TestCase("'!><", "#$%^", ExpectedResult = "'!#$@company.com")]
-        public string CanGetEmailFor(string firstName, string lastName) =>
+        public string CanGetEmailFor_UsingFunctionComposition(string firstName, string lastName) =>
             Test(
                 arrange: _ => new Person(firstName, lastName),
-                act: arrangedData => PersonComponent_FuncComp.EmailFor(arrangedData));
+                act: arrangedData => EmailFor(arrangedData));
+
+        [TestCase("Bilbo", "Baggins", ExpectedResult = "biba")]
+        [TestCase("123", "456", ExpectedResult = "1245")]
+        [TestCase("'!><", "#$%^", ExpectedResult = "'!#$")]
+        public string CanAbbreviateName_UsingMethodChaining(string firstName, string lastName) =>
+            Test(
+                arrange: _ => new Person(firstName, lastName),
+                act: arrangedData => arrangedData.AbbreviateName());
+
+        [TestCase("Bilbo", "Baggins", ExpectedResult = "biba@company.com")]
+        [TestCase("123", "456", ExpectedResult = "1245@company.com")]
+        [TestCase("'!><", "#$%^", ExpectedResult = "'!#$@company.com")]
+        public string CanGetEmailFor_UsingMethodChaining(string firstName, string lastName) =>
+            Test(
+                arrange: _ => new Person(firstName, lastName),
+                act: arrangedData => arrangedData.AbbreviateName().AppendDomain());
     }
 }
