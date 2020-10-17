573f9de1f0964b26686c7141eebc202fe25dfc2f
te25son <te25son@gmail.com>
2020-10-17 20:13:12 +0200

    Add richest quartile function with split methods

--- Examples.Tests/Chapter05/PopulationStatisticsTests.cs
+++ Examples.Tests/Chapter05/PopulationStatisticsTests.cs
@@ -6,6 +6,7 @@ namespace Examples.Tests.Chapter05
 {
     using static Enumerable;
     using static PopulationStatistics;
+    using static PopulationStatistics_Split;
 
     public sealed class PopulationStatisticsTests : TestFixture
     {
@@ -16,5 +17,13 @@ namespace Examples.Tests.Chapter05
                     .Select(i => new Person { Earnings = i * 10000 })
                     .ToList(),
                 act: arrangedData => AverageEarningsOfRichestQuartile(arrangedData));
+
+        [TestCase(ExpectedResult = 75000)]
+        public decimal AverageEarningsOfRichestQuartile_SplitTest() =>
+            Test(
+                arrange: _ => Range(1, 8)
+                    .Select(i => new Person { Earnings = i * 10000 })
+                    .ToList(),
+                act: arrangedData => AverageEarningsOfRichestQuartile_Split(arrangedData));
     }
 }
--- Examples/Chapter05/PopulationStatistics.cs
+++ Examples/Chapter05/PopulationStatistics.cs
@@ -12,4 +12,21 @@ namespace Examples.Chapter05
                 .Select(p => p.Earnings)
                 .Average();
     }
+
+    public static class PopulationStatistics_Split
+    {
+        /// <summary>
+        /// Splitting the function makes it more readable.
+        /// </summary>
+        public static decimal AverageEarningsOfRichestQuartile_Split(List<Person> population) =>
+            population
+                .RichestQuartile()
+                .AverageEarnings();
+
+        public static decimal AverageEarnings(this IEnumerable<Person> population) =>
+            population.Average(p => p.Earnings);
+
+        public static IEnumerable<Person> RichestQuartile(this List<Person> population) =>
+            population.OrderByDescending(p => p.Earnings).Take(population.Count() / 4);
+    }
 }
