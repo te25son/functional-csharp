5e72be033647a4d2bbb3464d8edeee01c36a3f9d
te25son <te25son@gmail.com>
2020-10-18 09:28:46 +0200

    Add make transfer controller example for imparative control flow

---
+++ Examples.Tests/Chapter05/BoC/MakeTransferController_Imparative.cs
@@ -0,0 +1,23 @@
+﻿using Examples.Chapter02.Banking;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Web.Mvc;
+
+namespace Examples.Tests.Chapter05.BoC.Imperative
+{
+    /// <summary>
+    /// Imparative approach
+    /// </summary>
+    public class MakeTransferController_Imparative : Controller
+    {
+        IValidator<MakeTransfer> validator;
+
+        public void MakeTransfer([FromBody] MakeTransfer transfer)
+        {
+            if (validator.IsValid(transfer))
+                Book(transfer);
+        }
+
+        void Book(MakeTransfer transfer) => throw new NotImplementedException();
+    }
+}
