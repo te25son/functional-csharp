9204ee752587addcada14867c2f13c1fbdf20446
te25son <te25son@gmail.com>
2020-10-18 08:39:09 +0200

    Clean up person tests

---
+++ Examples.Tests/Chapter05/PersonTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using Examples.Chapter05;
+
+namespace Examples.Tests.Chapter05
+{
+    public class PersonTestFixture : TestFixture
+    {
+        public Person CreatePerson(string firstName, string lastName) =>
+            new Person { FirstName = firstName, LastName = lastName };
+    }
+}
