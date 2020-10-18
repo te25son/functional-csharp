cdad80c44e3569b8a123d2b37fa68f85aaa5c2ad
te25son <te25son@gmail.com>
2020-10-18 09:53:40 +0200

    Add make transfer controller example for functional control flow

---
+++ Examples.Tests/Chapter05/BoC/MakeTransferController_Functional.cs
@@ -0,0 +1,22 @@
+﻿using Examples.Chapter02.Banking;
+using Functional;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Web.Mvc;
+
+namespace Examples.Tests.Chapter05.BoC
+{
+    using static Functional.F;
+
+    class MakeTransferController_Functional : Controller
+    {
+        IValidator<MakeTransfer> validator;
+
+        public void MakeTransfer([FromBody] MakeTransfer transfer) =>
+            Some(transfer)
+                .Where(validator.IsValid)
+                .ForEach(Book);
+
+        void Book(MakeTransfer transfer) => throw new NotImplementedException();
+    }
+}
