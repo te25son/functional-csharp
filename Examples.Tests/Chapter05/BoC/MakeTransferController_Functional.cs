548b431d86e10a3ad93c091f5d5943d446d41016
te25son <te25son@gmail.com>
2020-10-21 08:26:34 +0200

    Add FP version of account

--- Examples.Tests/Chapter05/BoC/MakeTransferController_Functional.cs
+++ Examples.Tests/Chapter05/BoC/MakeTransferController_Functional.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Web.Mvc;
 
-namespace Examples.Tests.Chapter05.BoC
+namespace Examples.Tests.Chapter05.BoC.Functional
 {
-    using static Functional.F;
+    using static F;
 
     class MakeTransferController_Functional : Controller
     {
@@ -21,5 +21,27 @@ namespace Examples.Tests.Chapter05.BoC
         void Book(MakeTransfer transfer) => throw new NotImplementedException();
 
         MakeTransfer Normalize(MakeTransfer transfer) => transfer;
+    }
+
+    /// <summary>
+    /// In FP the data and behavior are separated. Data is considered "dumb"
+    /// in this case and so its state cannot be changed.
+    /// </summary>
+    public class Account
+    {
+        public decimal Balance { get; }
+
+        public Account(decimal balance)
+        {
+            Balance = balance;
+        }
+    }
+
+    public static class AccountBehavior
+    {
+        public static Option<Account> Debit(this Account account, decimal amount) =>
+            (account.Balance < amount)
+                ? None
+                : Some(new Account(account.Balance - amount));
     }
 }
