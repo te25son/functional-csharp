7a68612a8fb8c286c440b23e4a9029fcd3f2821e
te25son <te25son@gmail.com>
2020-10-21 08:18:30 +0200

    Add OOP version of account

--- Examples.Tests/Chapter05/BoC/MakeTransferController_Imparative.cs
+++ Examples.Tests/Chapter05/BoC/MakeTransferController_Imparative.cs
@@ -20,4 +20,26 @@ namespace Examples.Tests.Chapter05.BoC.Imperative
 
         void Book(MakeTransfer transfer) => throw new NotImplementedException();
     }
+
+    /// <summary>
+    /// In OOP, data and behavior live in the same object,
+    /// and methods can sometimes change the objects state.
+    /// </summary>
+    public class Account
+    {
+        public decimal Balance { get; private set; }
+
+        public Account(decimal balance)
+        {
+            Balance = balance;
+        }
+
+        public void Debit(decimal amount)
+        {
+            if (Balance < amount)
+                throw new InvalidOperationException("Insufficient funds");
+
+            Balance -= amount;
+        }
+    }
 }
