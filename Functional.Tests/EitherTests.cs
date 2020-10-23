04f4d02d5978887b0e3a7a617af3f423ad610f7e
te25son <te25son@gmail.com>
2020-10-23 21:39:43 +0200

    Wip

--- Functional.Tests/EitherTests.cs
+++ Functional.Tests/EitherTests.cs
@@ -1,13 +1,32 @@
 ﻿using Examples.Tests;
+using NUnit.Framework;
 
 namespace Functional.Tests
 {
+    using static F;
+
     public sealed class EitherTests : TestFixture
     {
+        [TestCase(12, ExpectedResult = "Right(12)")]
+        [TestCase("hello", ExpectedResult = "Right(hello)")]
+        [TestCase(true, ExpectedResult = "Right(True)")]
         public string RightCanBeConvertedToString(object value)
         {
-            Test(
-                _ => Right())
+            return Test(
+                _ => Right(value),
+                right => right.ToString()
+            );
+        }
+
+        [TestCase(12, ExpectedResult = "Left(12)")]
+        [TestCase("hello", ExpectedResult = "Left(hello)")]
+        [TestCase(true, ExpectedResult = "Left(True)")]
+        public string LeftCanBeConvertedToString(object value)
+        {
+            return Test(
+                _ => Left(value),
+                left => left.ToString()
+            );
         }
     }
 }
