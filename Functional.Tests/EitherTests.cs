077a786763a41f022d6c58da3918c9a9901025dd
te25son <te25son@gmail.com>
2020-10-23 21:33:07 +0200

    Add either tests

---
+++ Functional.Tests/EitherTests.cs
@@ -0,0 +1,13 @@
+﻿using Examples.Tests;
+
+namespace Functional.Tests
+{
+    public sealed class EitherTests : TestFixture
+    {
+        public string RightCanBeConvertedToString(object value)
+        {
+            Test(
+                _ => Right())
+        }
+    }
+}
