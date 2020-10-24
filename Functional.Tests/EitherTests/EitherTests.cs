6d29c6c8a22ca5dfaf8a4f4e700b8021267670cb
te25son <te25son@gmail.com>
2020-10-24 09:41:36 +0200

    Add calc tests

--- Functional.Tests/EitherTests/EitherTests.cs
+++ Functional.Tests/EitherTests/EitherTests.cs
@@ -8,5 +8,15 @@ namespace Functional.Tests
 
     public sealed class EitherTests : EitherTestFixture
     {
+        [TestCase(3, 0, ExpectedResult = "Left(y cannot be 0)")]
+        [TestCase(-3, 3, ExpectedResult = "Left(x / y cannot be negative)")]
+        [TestCase(-3, -3, ExpectedResult = "Right(1)")]
+        [TestCase(3, 3, ExpectedResult = "Right(1)")]
+        public string CalcReturnsCorrectResponse(double x, double y)
+        {
+            return Test(
+                arrange: _ => Calc(x, y),
+                act: either => either.ToString());
+        }
     }
 }
