5949fb4d4fc7fc139b80548abe4ba386a1f3ffd9
te25son <te25son@gmail.com>
2020-10-25 12:58:28 +0100

    Add recruitment option example

---
+++ Examples/Chapter06/Recruitment.cs
@@ -0,0 +1,24 @@
+﻿using Functional;
+using System;
+
+namespace Examples.Chapter06
+{
+    using static F;
+
+    public class Recruitment_OptionExample
+    {
+        Func<Candidate, bool> IsEligible;
+        Func<Candidate, Option<Candidate>> TechTest;
+        Func<Candidate, Option<Candidate>> Interview;
+
+        Option<Candidate> Recruit(Candidate candidate) =>
+            Some(candidate)
+                .Where(IsEligible)
+                .Bind(TechTest)
+                .Bind(Interview);
+    }
+
+    public class Candidate 
+    {
+    }
+}
