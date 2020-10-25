7430880ce38ea76307453754066905a408239057
te25son <te25son@gmail.com>
2020-10-25 21:28:11 +0100

    Add make a meal workflow example

---
+++ Examples/Chapter06/MakeAMeal.cs
@@ -0,0 +1,44 @@
+﻿using Functional;
+using Functional.Either;
+using System;
+
+namespace Examples.Chapter06
+{
+    class MakeAMeal 
+    {
+        Func<Either<Reason, Unit>> WakeUpEarly;
+        Func<Unit, Either<Reason, Ingredients>> ShopForIngredients;
+        Func<Ingredients, Either<Reason, Food>> CookRecipe;
+
+        Action<Food> EnjoyTogether;
+        Action<Reason> ComplainAbout;
+        Action OrderPizza;
+
+        void Run()
+        {
+            WakeUpEarly()
+                .Bind(ShopForIngredients)
+                .Bind(CookRecipe)
+                .Match(
+                    right: dish => EnjoyTogether(dish),
+                    left: reason =>
+                    {
+                        ComplainAbout(reason);
+                        OrderPizza();
+                    }
+                );
+        }
+    }
+
+    class Reason 
+    {
+    }
+
+    class Ingredients 
+    {
+    }
+
+    class Food 
+    {
+    }
+}
