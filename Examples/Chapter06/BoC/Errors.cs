3ab0552d02608647c2bd8b80a9d8aa9f3704a890
te25son <te25son@gmail.com>
2020-10-27 17:01:27 +0100

    Set up errors for boc example

---
+++ Examples/Chapter06/BoC/Errors.cs
@@ -0,0 +1,23 @@
+﻿using Functional;
+
+namespace Examples.Chapter06.BoC
+{
+    public static class Errors
+    {
+        public static InvalidBic InvalidBic => new InvalidBic();
+
+        public static TransferDateIsPast TransferDateIsPast => new TransferDateIsPast();
+    }
+
+    public sealed class InvalidBic : Error
+    {
+        public override string Message { get; }
+            = "The beneficiary's BIC/Swift code is invalid";
+    }
+
+    public sealed class TransferDateIsPast : Error
+    {
+        public override string Message { get; }
+            = "Transfer date cannot be in the past";
+    }
+}
