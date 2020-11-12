98b34dd0a1f8a315e1200605e12ec3a3fc56f39c
te25son <te25son@gmail.com>
2020-11-12 15:55:26 +0100

    Added Result DTO

---
+++ Examples/Chapter06/BoC/Result.cs
@@ -0,0 +1,19 @@
+﻿using Functional;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Chapter06.BoC
+{
+    public class ResultDto<T>
+    {
+        public bool Succeeded { get; }
+        public bool Failed => !Succeeded;
+
+        public T Data { get; }
+        public Error Error { get; }
+
+        public ResultDto(T data) { Succeeded = true; Data = data; }
+        public ResultDto(Error error) { Error = error; }
+    }
+}
